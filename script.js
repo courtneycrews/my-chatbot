5e1e745428e7b11890d411ec1ebfa3aa6e8e6cc0
courtney <crews@smu.edu>
2024-09-15 16:39:55 -0700

    Fixed API fetching error

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ async function sendMessage() {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer API_KEY'
+            'Authorization': 'Bearer ${API_KEY}'
         },
         body: JSON.stringify({
             model: 'gpt-3.5-turbo',
